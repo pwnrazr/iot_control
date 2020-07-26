ddbd40ee61eaac28ca38a5a45eab2c8c8804fc68
Amir <amirz98@hotmail.com>
2020-07-26 14:07:57 +0800

    Complete refactor of code
    Replace deprecated ASyncTask with Thread instead
    Reworked comms class
    - properly utilized OOP things
    - removed unneeded getInputStream
    - made it more modular
    - removed need for global variables
    - no longer need separate class for esp32/node_relay
    - don't disconnect after running (might help performance?)
    As far as I understand it this makes it so theres a different http thread thing for each relay/esp32 thing which significantly increases performance

--- app/src/main/java/com/pwnrazr/iotcontrol/MainActivity.java
+++ app/src/main/java/com/pwnrazr/iotcontrol/MainActivity.java
@@ -2,86 +2,45 @@ package com.pwnrazr.iotcontrol;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.AsyncTask;
 import android.os.Bundle;
 import android.os.CountDownTimer;
 import android.util.Log;
 import android.widget.CompoundButton;
-import android.widget.TextView;
 import android.widget.ToggleButton;
 
-import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
 
 public class MainActivity extends AppCompatActivity {
-    // Global variables
-    static String esp32_receiveMsg = "";
-    static String relay_node_receiveMsg = "";
-    static String[] esp32_receiveMsg_split;
-    static String[] relay_node_receiveMsg_split;
+    public static class http_comm extends Thread {  // Communication to ESP8266 relay node
+        String receiveMsg = "";
+        String[] receiveMsg_split = {"",""};
+        boolean updateReady = false;
 
-    static boolean esp32_updateReady = false;
-    static boolean relay_node_updateReady = false;
-
-    static class esp32_comm extends AsyncTask<String, Void, String> {  // Communication to ESP32 nodeMCU
-        @Override
-        protected String doInBackground(String... params) {
-            try {
-                URL esp32_url = new URL("http://192.168.1.179/?" + params[0]);
-                HttpURLConnection esp32_connection = (HttpURLConnection) esp32_url.openConnection();
-
-                Log.i("iot_control",Integer.toString(esp32_connection.getResponseCode()));
-                Log.i("iot_control",esp32_connection.getResponseMessage());
-                esp32_receiveMsg = esp32_connection.getResponseMessage();
-                esp32_receiveMsg_split = esp32_receiveMsg.split(",");  //splits receiveMsg with comma(,) as separator
-                esp32_updateReady = true;
-
-                BufferedReader in = new BufferedReader(new InputStreamReader(esp32_connection.getInputStream()));
-                StringBuilder result = new StringBuilder();
-                String inputLine;
-                while ((inputLine = in.readLine()) != null)
-                    result.append(inputLine).append("\n");
-
-                in.close();
-                esp32_connection.disconnect();
-                return result.toString();
-            } catch (IOException e) {
-                Log.e("iot_control",e.toString());
-            }
-
-            return null;
+        String url = "";
+        http_comm(String url){
+            this.url = url;
         }
-    }
-    static class relay_node_comm extends AsyncTask<String, Void, String> {  // Communication to ESP8266 relay node
-        @Override
-        protected String doInBackground(String... params) {
-            try {
-                URL relay_node_url = new URL("http://192.168.1.161/?" + params[0]);
-                HttpURLConnection relay_node_connection = (HttpURLConnection) relay_node_url.openConnection();
+        void execute(final String... command) {
+            new Thread() {
+                public void run() {
+                    try {
+                        URL relay_node_url = new URL("http://" + url + "/?" + command[0]);
+                        HttpURLConnection relay_node_connection = (HttpURLConnection) relay_node_url.openConnection();
 
-                Log.i("iot_control",Integer.toString(relay_node_connection.getResponseCode()));
-                Log.i("iot_control",relay_node_connection.getResponseMessage());
-                relay_node_receiveMsg = relay_node_connection.getResponseMessage();
-                relay_node_receiveMsg_split = relay_node_receiveMsg.split(",");  //splits receiveMsg with comma(,) as separator
-                relay_node_updateReady = true;
+                        Log.i("iot_control", Integer.toString(relay_node_connection.getResponseCode()));
+                        Log.i("iot_control", relay_node_connection.getResponseMessage());
+                        receiveMsg = relay_node_connection.getResponseMessage();
+                        receiveMsg_split = receiveMsg.split(",");  //splits receiveMsg with comma(,) as separator
+                        updateReady = true;
 
-                BufferedReader in = new BufferedReader(new InputStreamReader(relay_node_connection.getInputStream()));
-                StringBuilder result = new StringBuilder();
-                String inputLine;
-                while ((inputLine = in.readLine()) != null)
-                    result.append(inputLine).append("\n");
-
-                in.close();
-                relay_node_connection.disconnect();
-                return result.toString();
-            } catch (IOException e) {
-                Log.e("iot_control",e.toString());
-            }
-
-            return null;
+                        //relay_node_connection.disconnect();       //potentially unneeded
+                    } catch (IOException e) {
+                        Log.e("iot_control", e.toString());
+                    }
+                }
+            }.start();
         }
     }
     @Override
@@ -91,12 +50,19 @@ public class MainActivity extends AppCompatActivity {
 
         // Declarations
         final ToggleButton esp32_led_toggle = findViewById(R.id.esp32_led_toggle);
-
         final ToggleButton relay0_toggle = findViewById(R.id.relay0_toggle);
         final ToggleButton relay1_toggle = findViewById(R.id.relay1_toggle);
         final ToggleButton relay2_toggle = findViewById(R.id.relay2_toggle);
         final ToggleButton relay3_toggle = findViewById(R.id.relay3_toggle);
 
+        //communications part
+        final http_comm esp32_comm_0 = new http_comm("192.168.1.183");
+        final http_comm relay_node_comm_0 = new http_comm("192.168.1.161");
+        final http_comm relay_node_comm_1 = new http_comm("192.168.1.161");
+        final http_comm relay_node_comm_2 = new http_comm("192.168.1.161");
+        final http_comm relay_node_comm_3 = new http_comm("192.168.1.161");
+
+        // Disable buttons
         esp32_led_toggle.setEnabled(false);
         relay0_toggle.setEnabled(false);
         relay1_toggle.setEnabled(false);
@@ -104,68 +70,73 @@ public class MainActivity extends AppCompatActivity {
         relay3_toggle.setEnabled(false);
 
         // on app startup get status of things
-        new esp32_comm().execute("reqled");     //Request status of LED on app startup
-
-        new relay_node_comm().execute("req_relay0");
-        new relay_node_comm().execute("req_relay1");
-        new relay_node_comm().execute("req_relay2");
-        new relay_node_comm().execute("req_relay3");
+        esp32_comm_0.execute("reqled");     //Request status of LED on app startup
+        relay_node_comm_0.execute("req_relay0");
+        relay_node_comm_1.execute("req_relay1");
+        relay_node_comm_2.execute("req_relay2");
+        relay_node_comm_3.execute("req_relay3");
 
         new CountDownTimer(5, 5)  //loop update buttons
         {
             public void onTick(long l) {}
             public void onFinish()
             {
-                if (esp32_updateReady) {   //Only update when there's actually something to update
-                    TextView debugText = findViewById(R.id.debugText);
-                    debugText.setText(String.format("%s=%s", esp32_receiveMsg_split[0], esp32_receiveMsg_split[1]));
-                    if (esp32_receiveMsg_split[0].equals("LED")) {
-                        if (esp32_receiveMsg_split[1].equals("ON")) {
+                if (esp32_comm_0.updateReady) {   //Only update when there's actually something to update
+                    if (esp32_comm_0.receiveMsg_split[0].equals("LED")) {
+                        if (esp32_comm_0.receiveMsg_split[1].equals("ON")) {
                             esp32_led_toggle.setChecked(true);
                         } else {
                             esp32_led_toggle.setChecked(false);
                         }
                         esp32_led_toggle.setEnabled(true);
+                        esp32_comm_0.updateReady = false;
                     }
-                    esp32_updateReady = false;
                 }
-                if (relay_node_updateReady) {
-                    TextView debugText = findViewById(R.id.debugText);
-                    debugText.setText(String.format("%s=%s", relay_node_receiveMsg_split[0], relay_node_receiveMsg_split[1]));
-                    if (relay_node_receiveMsg_split[0].equals("relay0")) {
-                        if (relay_node_receiveMsg_split[1].equals("on")) {
+                if(relay_node_comm_0.updateReady) {
+                    if (relay_node_comm_0.receiveMsg_split[0].equals("relay0")) {
+                        if (relay_node_comm_0.receiveMsg_split[1].equals("on")) {
                             relay0_toggle.setChecked(true);
                         } else {
                             relay0_toggle.setChecked(false);
                         }
                         relay0_toggle.setEnabled(true);
+                        relay_node_comm_0.updateReady = false;
                     }
-                    if (relay_node_receiveMsg_split[0].equals("relay1")) {
-                        if (relay_node_receiveMsg_split[1].equals("on")) {
+                }
+                if(relay_node_comm_1.updateReady) {
+                    if (relay_node_comm_1.receiveMsg_split[0].equals("relay1")) {
+                        if (relay_node_comm_1.receiveMsg_split[1].equals("on")) {
                             relay1_toggle.setChecked(true);
                         } else {
                             relay1_toggle.setChecked(false);
                         }
                         relay1_toggle.setEnabled(true);
+                        relay_node_comm_1.updateReady = false;
                     }
-                    if (relay_node_receiveMsg_split[0].equals("relay2")) {
-                        if (relay_node_receiveMsg_split[1].equals("on")) {
+                }
+                if(relay_node_comm_2.updateReady) {
+                    if (relay_node_comm_2.receiveMsg_split[0].equals("relay2")) {
+                        if (relay_node_comm_2.receiveMsg_split[1].equals("on")) {
                             relay2_toggle.setChecked(true);
                         } else {
                             relay2_toggle.setChecked(false);
                         }
                         relay2_toggle.setEnabled(true);
+                        relay_node_comm_2.updateReady = false;
                     }
-                    if (relay_node_receiveMsg_split[0].equals("relay3")) {
-                        if (relay_node_receiveMsg_split[1].equals("on")) {
+                }
+                if(relay_node_comm_3.updateReady) {
+                    if (relay_node_comm_3.receiveMsg_split[0].equals("relay3")) {
+                        if (relay_node_comm_3.receiveMsg_split[1].equals("on")) {
                             relay3_toggle.setChecked(true);
                         } else {
                             relay3_toggle.setChecked(false);
                         }
                         relay3_toggle.setEnabled(true);
+                        relay_node_comm_3.updateReady = false;
                     }
-                    relay_node_updateReady = false;
                 }
+                //////
                 start();
             }
         }.start();
@@ -174,9 +145,9 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onCheckedChanged(CompoundButton compoundButton, boolean b) {
                 if(b){
-                    new esp32_comm().execute("led1=1");
+                    esp32_comm_0.execute("led1=1");
                 } else {
-                    new esp32_comm().execute("led1=0");
+                    esp32_comm_0.execute("led1=0");
                 }
             }
         });
@@ -185,9 +156,9 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onCheckedChanged(CompoundButton compoundButton, boolean b) {
                 if(b){
-                    new relay_node_comm().execute("relay0=1");
+                    relay_node_comm_0.execute("relay0=1");
                 } else {
-                    new relay_node_comm().execute("relay0=0");
+                    relay_node_comm_0.execute("relay0=0");
                 }
             }
         });
@@ -196,9 +167,9 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onCheckedChanged(CompoundButton compoundButton, boolean b) {
                 if(b){
-                    new relay_node_comm().execute("relay1=1");
+                    relay_node_comm_1.execute("relay1=1");
                 } else {
-                    new relay_node_comm().execute("relay1=0");
+                    relay_node_comm_1.execute("relay1=0");
                 }
             }
         });
@@ -207,9 +178,9 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onCheckedChanged(CompoundButton compoundButton, boolean b) {
                 if(b){
-                    new relay_node_comm().execute("relay2=1");
+                    relay_node_comm_2.execute("relay2=1");
                 } else {
-                    new relay_node_comm().execute("relay2=0");
+                    relay_node_comm_2.execute("relay2=0");
                 }
             }
         });
@@ -218,9 +189,9 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onCheckedChanged(CompoundButton compoundButton, boolean b) {
                 if(b){
-                    new relay_node_comm().execute("relay3=1");
+                    relay_node_comm_3.execute("relay3=1");
                 } else {
-                    new relay_node_comm().execute("relay3=0");
+                    relay_node_comm_3.execute("relay3=0");
                 }
             }
         });
